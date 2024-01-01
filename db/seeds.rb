2ef4d5a621758eb2e1ddd64001b493dba51460c8
Ferenc Nagy <ferenc.nagy.jr@protonmail.com>
2024-01-01 15:54:57 -0500

    add some documentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# To create test user in the test environment
+# User.create!(email: 'admin@example.com', password_digest: 'admin')
