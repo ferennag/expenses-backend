c4ab0848f577f4444f2a49786ef69f3b9cb5d3b8
Ferenc Nagy <ferenc.nagy.jr@protonmail.com>
2024-01-02 11:59:06 -0500

    Add rule engine frame, and todos

---
+++ app/services/rule_engine/evaluator.rb
@@ -0,0 +1,13 @@
+module RuleEngine
+  # TODO Rule engine implementation
+  # - Save rules in DB
+  # - Allow rules to evaluate a specific field of an object, and determine the action to do
+  # - Categorization action: categorize a transaction based on a rule
+  # - Allow users to create, update, and delete rules
+  # - Rules are specific to a workspace
+
+  class Evaluator
+    def perform
+    end
+  end
+end
